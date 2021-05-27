7450a88cedd451f31ef206c02a2058dc8ead31e6
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-05-27 08:22:09 +0300

    Update demo app struct

---
+++ VGSCheckoutDemoApp/VGSCheckoutDemoApp/Application/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  VGSCheckoutDemoApp
+//
+//  Created by Dima on 26.05.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
+
