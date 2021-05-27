7fee5fce3f3b44ec7ad810fb37b4df79a8f91b79
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-05-27 16:05:23 +0300

    Revert naming

--- VGSCheckoutDemoApp/VGSCheckoutDemoApp/Models/OrderDataProvider/OrderDataProvider.swift
+++ VGSCheckoutDemoApp/VGSCheckoutDemoApp/Models/OrderDataProvider/OrderDataProvider.swift
@@ -8,15 +8,14 @@ import Foundation
 import UIKit
 #endif
 
-
 /// Order data provider.
 final class OrderDataProvider {
 	/// Provides order items.
 	/// - Returns: An array of `OrderItem` objects.
 	static func provideOrders() -> [OrderItem] {
 		return [
-			OrderItem(title: "Pizza Mars", image: UIImage(named: "pizza_diablo"), price: 9.90),
-			OrderItem(title: "Alien Noodles", image: UIImage(named: "pizza_vegano"), price: 44.95)
+			OrderItem(title: "Pizza Diablo", image: UIImage(named: "pizza_diablo"), price: 9.90),
+			OrderItem(title: "Pizza Vegano", image: UIImage(named: "pizza_vegano"), price: 44.95)
 		]
 	}
 }
