4ab8b0d638f603c4cad2cedfa7dec40a5fdbe349
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-05-31 13:30:08 +0300

    Add http methods

--- Sources/VGSCheckout/Scanners/CardIO/CheckoutCardIOScanner.swift
+++ Sources/VGSCheckout/Scanners/CardIO/CheckoutCardIOScanner.swift
@@ -70,17 +70,3 @@ internal class CheckoutViewController: UIViewController {
 		}
 	}
 }
-
-/// HTTP request methods
-public enum VGSCheckoutHTTPMethod: String {
-		/// GET method.
-		case get     = "GET"
-		/// POST method.
-		case post    = "POST"
-		/// PUT method.
-		case put     = "PUT"
-		/// PATCH method.
-		case patch = "PATCH"
-		/// DELETE method.
-		case delete = "DELETE"
-}
