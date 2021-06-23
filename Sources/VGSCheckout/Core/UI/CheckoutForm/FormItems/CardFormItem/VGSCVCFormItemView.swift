57b343d85da14a89c766de0c41b5df79eae43fa0
Dima Khludkov <57712402+dmytrokhl@users.noreply.github.com>
2021-06-23 16:37:08 +0300

    Smart CVC Placeholder

--- Sources/VGSCheckout/Core/UI/CheckoutForm/FormItems/CardFormItem/VGSCVCFormItemView.swift
+++ Sources/VGSCheckout/Core/UI/CheckoutForm/FormItems/CardFormItem/VGSCVCFormItemView.swift
@@ -52,7 +52,7 @@ internal class VGSCVCFormItemView: UIView, VGSTextFieldFormItemProtocol {
 		formItemView.translatesAutoresizingMaskIntoConstraints = false
 		formItemView.checkout_constraintViewToSuperviewEdges()
 
-		formItemView.hintComponentView.label.text = "CVC"
+		formItemView.hintComponentView.label.text = "Security code"
 		formItemView.stackView.addArrangedSubview(cvcTextField)
 	}
 }
