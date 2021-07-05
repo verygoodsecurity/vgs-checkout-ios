5be1dc87fb9a50df5d9baa6d28d2571d3c2ac98f
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-07-05 09:53:40 +0300

    Fix linter warnings and naming

--- Sources/VGSCheckout/Core/Checkout/UseCase/AddCard/FormValidation/FieldValidators/VGSCardNumberFieldValidator.swift
+++ Sources/VGSCheckout/Core/Checkout/UseCase/AddCard/FormValidation/FieldValidators/VGSCardNumberFieldValidator.swift
@@ -6,24 +6,22 @@ import Foundation
 
 internal class VGSCardNumberFieldValidator: VGSFormTextFieldValidationProtocol {
 
-  internal let DEFAULT_ANY_CARD_LENGHT = 16
-  internal let DEFAULT_AMEX_CARD_LENGHT = 15
+	/// Default card number length.
+  internal let defaultAnyCardLength = 16
     
 	internal func isTextFieldInputComplete(_ textField: VGSTextField) -> Bool {
 
 		let inputLength = textField.state.inputLength
     
 		if let cardState = textField.state as? CardState {
-      let maxCardBrandLength = getMaxValidLengthForCardBrand(cardState.cardBrand)
+			let maxCardBrandLength = getMaxValidLength(for: cardState.cardBrand)
       return inputLength == maxCardBrandLength
 		} else {
-			return inputLength == DEFAULT_ANY_CARD_LENGHT
+			return inputLength == defaultAnyCardLength
 		}
 	}
   
-  internal func getMaxValidLengthForCardBrand(_ cardBrand: VGSCheckoutPaymentCards.CardBrand) -> Int {
-    return cardBrand.cardLengths.max() ?? DEFAULT_ANY_CARD_LENGHT
+  internal func getMaxValidLength(for cardBrand: VGSCheckoutPaymentCards.CardBrand) -> Int {
+    return cardBrand.cardLengths.max() ?? defaultAnyCardLength
   }
-
-
 }
