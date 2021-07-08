16ee84781d6f2c8bfd08278764dfb0c3722c33fe
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-07-08 15:51:07 +0300

    Add region JSON parser

--- Sources/VGSCheckout/Core/Collect/CollectUIElements/Text Field/PickerFields/AddressDataProviders/VGSStateDataSourceProvider.swift
+++ Sources/VGSCheckout/Core/Collect/CollectUIElements/Text Field/PickerFields/AddressDataProviders/VGSStateDataSourceProvider.swift
@@ -12,23 +12,63 @@ internal enum VGSAddressRegionType {
 	case general
 }
 
-internal protocol VGSBaseRegionModelProtocol {
-	var regionType: VGSAddressRegionType {get}
-	var displayName: String {get set}
+internal extension JsonData {
+
+	init?(jsonFileName: String) {
+		let notFoundCompletion = {
+			print("JSON file \(jsonFileName).json not found or is invalid")
+		}
+
+		guard let bundle = BundleUtils.shared.resourcesBundle else {
+			assertionFailure("Bundle not found!")
+			return nil
+		}
+
+		if let path = bundle.path(forResource: jsonFileName, ofType: "json") {
+			do {
+				let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+				let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+				guard let json = jsonResult as? JsonData else {
+					notFoundCompletion()
+					return nil
+				}
+
+				self = json
+			} catch {
+				notFoundCompletion()
+				return nil
+			}
+		} else {
+			notFoundCompletion()
+			return nil
+		}
+	}
 }
 
-internal class VGSRegionModelUS: VGSBaseRegionModelProtocol {
-	internal var regionType: VGSAddressRegionType {
-		return .state
+internal struct VGSAddressRegionModel {
+	var regionType: VGSAddressRegionType = .region
+	let displayName: String
+	let code: String
+
+	static let codeKey = "abbreviation"
+	static let nameKey = "name"
+
+	init?(regionFileName: String) {
+		guard let json = JsonData.init(jsonFileName: regionFileName) else {
+			return nil
+		}
+
+		guard let name = json[VGSAddressRegionModel.codeKey] as? String, let code =  json[VGSAddressRegionModel.nameKey] as? String else {
+			return nil
+		}
+
+		self.displayName = name
+		self.code = code
 	}
-
-	internal var displayName: String = ""
-
-	internal let code: String = ""
 }
 
 internal class VGSAddressRegionProvider {
-	static func provideRegions(for countryCode: String) -> [VGSBaseRegionModelProtocol] {
+	static func provideRegions(for countryCode: String) -> [VGSAddressRegionModel] {
 
 		let iso = VGSCountriesISO.self
 		switch countryCode {
