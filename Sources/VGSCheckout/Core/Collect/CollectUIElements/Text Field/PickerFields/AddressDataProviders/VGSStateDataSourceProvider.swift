9c3553208cd40182e88b51ae8812b6ae23b28113
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-07-08 17:07:36 +0300

    Map data for regions

--- Sources/VGSCheckout/Core/Collect/CollectUIElements/Text Field/PickerFields/AddressDataProviders/VGSStateDataSourceProvider.swift
+++ Sources/VGSCheckout/Core/Collect/CollectUIElements/Text Field/PickerFields/AddressDataProviders/VGSStateDataSourceProvider.swift
@@ -12,6 +12,28 @@ internal enum VGSAddressRegionType {
 	case general
 }
 
+internal typealias JSONArray = [Any]
+
+extension JSONArray {
+	init?(jsonFileName: String) {
+
+		guard let bundle = BundleUtils.shared.resourcesBundle, let path = bundle.path(forResource: jsonFileName, ofType: "json") else {
+			assertionFailure("Bundle not found!")
+			return nil
+		}
+
+		do {
+			guard let jsonArray = try JSONSerialization.jsonObject(with: Data(contentsOf: URL(fileURLWithPath: path)), options: JSONSerialization.ReadingOptions()) as? [Any] else {
+				return nil
+			}
+
+			self = jsonArray
+		} catch {
+			return nil
+		}
+	}
+}
+
 internal extension JsonData {
 
 	init?(jsonFileName: String) {
@@ -53,11 +75,7 @@ internal struct VGSAddressRegionModel {
 	static let codeKey = "abbreviation"
 	static let nameKey = "name"
 
-	init?(regionFileName: String) {
-		guard let json = JsonData.init(jsonFileName: regionFileName) else {
-			return nil
-		}
-
+	init?(json: JsonData) {
 		guard let name = json[VGSAddressRegionModel.codeKey] as? String, let code =  json[VGSAddressRegionModel.nameKey] as? String else {
 			return nil
 		}
@@ -70,18 +88,50 @@ internal struct VGSAddressRegionModel {
 internal class VGSAddressRegionProvider {
 	static func provideRegions(for countryCode: String) -> [VGSAddressRegionModel] {
 
-		let iso = VGSCountriesISO.self
-		switch countryCode {
-		case iso.us.rawValue:
-			return []
-		case iso.ca.rawValue:
-			return []
-		default:
+		guard let fileName = regionFileName(for: countryCode), let jsonArray = JSONArray(jsonFileName: fileName)  else {
 			return []
 		}
 
-		return [
+		var regions = [VGSAddressRegionModel]()
 
-		]
+		for item in jsonArray {
+			if let json = item as? JsonData {
+				if let region = VGSAddressRegionModel(json: json) {
+					regions.append(region)
+				}
+			}
+		}
+
+		return regions
+	}
+
+	static func regionFileName(for countryCode: String) -> String? {
+		let iso = VGSCountriesISO.self
+
+		switch countryCode {
+		case iso.us.rawValue:
+			return "VGSUnitedStatesRegions"
+		case iso.ca.rawValue:
+			return "VGSCanadaRegions"
+		default:
+			return nil
+		}
+	}
+
+	static func regionType(for countryCode: String) -> VGSAddressRegionType {
+		let countryISO = countryISO(for: countryCode)
+
+		switch countryISO {
+		case .us:
+			return .state
+		case .ca:
+			return .province
+		default:
+			return .general
+		}
+	}
+
+	static func countryISO(for countryCode: String) -> VGSCountriesISO {
+		return VGSCountriesISO(rawValue: countryCode) ?? .us
 	}
 }
