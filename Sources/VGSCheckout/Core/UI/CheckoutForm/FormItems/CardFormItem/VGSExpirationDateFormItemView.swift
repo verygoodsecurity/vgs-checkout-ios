0566cce63a8d4809091cae1d8b1ec1ef1d036f4f
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-07-15 10:54:45 +0300

    Move form item view protocol to separate file

--- Sources/VGSCheckout/Core/UI/CheckoutForm/FormItems/CardFormItem/VGSExpirationDateFormItemView.swift
+++ Sources/VGSCheckout/Core/UI/CheckoutForm/FormItems/CardFormItem/VGSExpirationDateFormItemView.swift
@@ -8,41 +8,6 @@ import Foundation
 import UIKit
 #endif
 
-internal enum VGSCheckoutFormValidationState {
-	case inactive
-	case focused
-	case valid
-	case invalid
-	case disabled
-}
-
-internal protocol VGSTextFieldFormItemProtocol: AnyObject {
-	var formItemView: VGSPlaceholderFormItemView {get}
-	var textField: VGSTextField {get}
-	var fieldType: VGSAddCardFormFieldType {get}
-  
-  func updateUI(for validationState: VGSCheckoutFormValidationState)
-}
-
-/// TODO: Move to base class?
-extension VGSTextFieldFormItemProtocol {
-  
-  // MARK: - Interface
-
-  /// Update UI.
-  /// - Parameter validationState: `VGSCheckoutFormValidationState` object, form validation state.
-  func updateUI(for validationState: VGSCheckoutFormValidationState) {
-    switch validationState {
-    case .focused, .inactive, .valid:
-      formItemView.hintComponentView.accessory = .none
-    case .invalid:
-      formItemView.hintComponentView.accessory = .invalid
-    case .disabled:
-      formItemView.hintComponentView.accessory = .none
-    }
-  }
-}
-
 internal class VGSExpirationDateFormItemView: UIView, VGSTextFieldFormItemProtocol {
 
 	// MARK: - Vars
