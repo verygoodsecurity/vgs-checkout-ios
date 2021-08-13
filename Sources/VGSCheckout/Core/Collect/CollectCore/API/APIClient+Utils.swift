0afb98c95486d14924fd549828b1b393689d7038
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-08-13 08:44:57 +0300

    Build multiplexing url

--- Sources/VGSCheckout/Core/Collect/CollectCore/API/APIClient+Utils.swift
+++ Sources/VGSCheckout/Core/Collect/CollectCore/API/APIClient+Utils.swift
@@ -29,11 +29,7 @@ internal extension APIClient {
 
 		// Check vault url is valid.
 		guard let url = URL(string: strUrl) else {
-			assertionFailure("❗VGSCheckout CONFIGURATION ERROR: : NOT VALID ORGANIZATION PARAMETERS!!!")
-
-			let eventText = "CONFIGURATION ERROR: NOT VALID ORGANIZATION PARAMETERS!!! tenantID: \(tenantId), environment: \(regionalEnvironment)"
-			let event = VGSLogEvent(level: .warning, text: eventText, severityLevel: .error)
-			VGSCollectLogger.shared.forwardLogEvent(event)
+			APIClient.logCannotBuildURLEvent(for: tenantId, regionalEnvironment: regionalEnvironment)
 
 			return nil
 		}
@@ -53,17 +49,53 @@ internal extension APIClient {
 		VGSCollectLogger.shared.forwardLogEvent(event)
 		assert(VGSCollect.tenantIDValid(vaultID), "❗VGSCheckout CONFIGURATION ERROR: : VAULT ID IS NOT VALID!!!")
 	}
+
+	static func logCannotBuildURLEvent(for vaultID: String, regionalEnvironment: String) {
+		assertionFailure("❗VGSCheckout CONFIGURATION ERROR: : NOT VALID ORGANIZATION PARAMETERS!!!")
+
+		let eventText = "CONFIGURATION ERROR: NOT VALID ORGANIZATION PARAMETERS!!! vaultID: \(vaultID), environment: \(regionalEnvironment)"
+		let event = VGSLogEvent(level: .warning, text: eventText, severityLevel: .error)
+		VGSCollectLogger.shared.forwardLogEvent(event)
+	}
 }
 
+/// Helper class to build URLs.
 internal final class VGSCheckoutVaultURLBuilder {
 
+	/// Builds vault URL. Multiplexing URL is different from general route setup.
+	/// - Parameters:
+	///   - paymentInstrument: `VGSPaymentInstrument` object, payment instrument.
+	///   - tenantId: `String` object, tenant ID.
+	///   - regionalEnvironment: `String` object, regional environment.
+	/// - Returns: `URL?` object, vault `URL` if configuration is valid or `nil`.
 	internal static func buildVaultURL(for paymentInstrument: VGSPaymentInstrument, tenantId: String, regionalEnvironment: String) -> URL? {
 
 		switch paymentInstrument {
-		case .vault(let configuration):
+		case .vault:
 			return APIClient.buildVaultURL(tenantId: tenantId, regionalEnvironment: regionalEnvironment)
-		case .multiplexing(let configuration):
-			return nil
+		case .multiplexing:
+			// Check environment is valid.
+			if !VGSCollect.regionalEnironmentStringValid(regionalEnvironment) {
+				APIClient.logInvalidEnironmentEvent(regionalEnvironment)
+			}
+
+			// Check tenant is valid.
+			if !VGSCollect.tenantIDValid(tenantId) {
+				APIClient.logInvalidVaultIDEvent(tenantId)
+			}
+
+      // https://<VAULT_ID>.multiplexing.<DATA_ENVIRONMENT>.verygoodsecurity.app/
+
+			let multiplexingURLStr = "https://" + tenantId + ".multiplexing." + regionalEnvironment + ".verygoodproxy.app"
+
+			// Check vault url is valid.
+			guard let url = URL(string: multiplexingURLStr) else {
+				APIClient.logCannotBuildURLEvent(for: tenantId, regionalEnvironment: regionalEnvironment)
+
+				return nil
+			}
+
+			return url
 		}
 	}
 }
