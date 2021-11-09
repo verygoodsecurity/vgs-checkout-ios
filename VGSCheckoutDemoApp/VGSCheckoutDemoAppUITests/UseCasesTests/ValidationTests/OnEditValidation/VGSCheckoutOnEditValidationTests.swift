29027d6d003d11285b2e2098484f4b2897fc49e7
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-11-09 09:54:49 +0200

    Add UI tests for change country flow

--- VGSCheckoutDemoApp/VGSCheckoutDemoAppUITests/UseCasesTests/ValidationTests/OnEditValidation/VGSCheckoutOnEditValidationTests.swift
+++ VGSCheckoutDemoApp/VGSCheckoutDemoAppUITests/UseCasesTests/ValidationTests/OnEditValidation/VGSCheckoutOnEditValidationTests.swift
@@ -74,6 +74,9 @@ class VGSCheckoutOnEditValidationTests: VGSCheckoutSaveCardBaseTestCase {
 
 		// Dismiss keyboard.
 		dismissKeyboardForCardDetails()
+
+		// Verifies postal code/zip errors are updated correctly on country change.
+		verifyUpdatePostalCodeErrorOnCountryChange()
 	}
 
 	// MARK: - Helpers
@@ -189,13 +192,40 @@ class VGSCheckoutOnEditValidationTests: VGSCheckoutSaveCardBaseTestCase {
 		// Swipe up to bottom.
 		app.swipeUp()
 
-		// Type valid zip code.
+		// Type invalid zip code.
 		VGSTextField.BillingAddress.zip.find(in: app).type("1234", shouldClear: true)
 
 		// Focus to city field.
 		VGSTextField.BillingAddress.city.find(in: app).tap()
 
-		// Verify invalid zip code error is not displayed.
+		// Verify invalid zip code error is displayed.
 		XCTAssertTrue(Labels.CheckoutErrorLabels.BillingAddress.invalidZIP.exists(in: app))
+	}
+
+	/// Verifies postal code/zip errors are updated correctly on country change.
+	func verifyUpdatePostalCodeErrorOnCountryChange() {
+		// Swipe up to bottom.
+		app.swipeUp()
+
+		// Type valid zip code.
+		VGSTextField.BillingAddress.zip.find(in: app).type("12345", shouldClear: true)
+
+		// Switch to Canada from the US.
+		selectCountry("Canada", currentCounryName: "United States")
+
+		// Verify postal code UI is displayed.
+		verifyPostalCodeUI()
+
+		// Verify postal code error is displayed.
+		XCTAssertTrue(Labels.CheckoutErrorLabels.BillingAddress.invalidPostalCode.exists(in: app))
+
+		// Switch to the US from Canada
+		selectCountry("United States", currentCounryName: "Canada")
+
+		// Verify zip code UI is displayed.
+		verifyZIPUI()
+
+		// Verify error for postal code is not displayed.
+		XCTAssertFalse(Labels.CheckoutErrorLabels.BillingAddress.invalidPostalCode.exists(in: app))
 	}
 }
