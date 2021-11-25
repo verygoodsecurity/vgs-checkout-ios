60de034630fa55c20375124079198cefa6ebb99b
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-11-25 13:59:11 +0200

    Fix typo in docs

--- Sources/VGSCheckoutSDK/Core/Checkout/UseCase/AddCard/CardDetails/VGSCardDataSectionViewModel.swift
+++ Sources/VGSCheckoutSDK/Core/Checkout/UseCase/AddCard/CardDetails/VGSCardDataSectionViewModel.swift
@@ -30,7 +30,7 @@ public enum VGSCheckoutFormValidationBehaviour {
 	/// Validate fields and display errors on submit.
 	case onSubmit
 
-	/// Validate fields and display errors on switching to the next field.
+	/// Validate fields and display errors on end editing. Revalidate form on submit.s
 	case onFocus
 
 	/// Analytics key name.
