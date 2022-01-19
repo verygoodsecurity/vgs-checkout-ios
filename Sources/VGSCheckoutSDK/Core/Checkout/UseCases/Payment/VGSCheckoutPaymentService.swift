e7c81548074541d9e625b3f35e063a00f1aab447
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2022-01-19 07:54:18 +0200

    Set pay with new card as initial flow

--- Sources/VGSCheckoutSDK/Core/Checkout/UseCases/Payment/VGSCheckoutPaymentService.swift
+++ Sources/VGSCheckoutSDK/Core/Checkout/UseCases/Payment/VGSCheckoutPaymentService.swift
@@ -46,16 +46,18 @@ internal class VGSCheckoutPayoptTransfersService: NSObject, VGSCheckoutServicePr
 		self.checkoutConfigurationType = checkoutConfigurationType
 		self.vgsCollect = vgsCollect
 		self.uiTheme = uiTheme
-		switch checkoutConfigurationType {
-			case .payoptTransfers(let configuration):
-				if configuration.savedCards.isEmpty {
-					self.initialScreen = .payWithNewCard
-				} else {
-					self.initialScreen = .paymentOptions
-				}
-			default:
-				fatalError("wrong flow")
-			}
+		self.initialScreen = .payWithNewCard
+//		switch checkoutConfigurationType {
+//
+//			case .payoptTransfers(let configuration):
+//				if configuration.savedCards.isEmpty {
+//					self.initialScreen = .payWithNewCard
+//				} else {
+//					self.initialScreen = .paymentOptions
+//				}
+//			default:
+//				fatalError("wrong flow")
+//			}
 	}
 
 	/// Builds view controller for save card flow.
@@ -65,6 +67,7 @@ internal class VGSCheckoutPayoptTransfersService: NSObject, VGSCheckoutServicePr
 		case .payWithNewCard:
 			return buildPayWithNewCardVC()
 		case .paymentOptions:
+			fatalError("not implemented")
 			return buildPaymentOptionsVC()
 		}
 	}
