a10c60e01832ade30d6af90f9810f3a61ea3c1af
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2022-04-15 06:34:07 +0300

    Remove unused method

--- Sources/VGSCheckoutSDK/Core/Checkout/CheckoutDelegate/VGSCheckoutDelegate.swift
+++ Sources/VGSCheckoutSDK/Core/Checkout/CheckoutDelegate/VGSCheckoutDelegate.swift
@@ -13,11 +13,6 @@ public protocol VGSCheckoutDelegate: AnyObject {
 	/// Tells the delegate that user cancelled checkout flow (closed checkout screen).
 	func checkoutDidCancel()
 
-	/// Tells the delegate that save card succeeded and fin instrument created.
-	/// - Parameter data: `Data?` object, holds response data with created financial instrument for saved card.
-	/// - Parameter response: `URLResponse?` object, holds URL response object with created financial instrument for saved card.
-//	func saveCardDidSuccess(with data: Data?, response: URLResponse?)
-
 	/// Tells the delegate that user removed saved card from payment options methods.
 	/// - Parameter id: `String` object, removed saved card financial instrument id.
 	/// - Parameter result: `VGSCheckoutRequestResult` object, remove card request result.
@@ -30,9 +25,6 @@ public protocol VGSCheckoutDelegate: AnyObject {
 
 // Defeault protocol implementation.
 public extension VGSCheckoutDelegate {
-
-	/// no:doc
-//	func saveCardDidSuccess(with data: Data?, response: URLResponse?) {}
 
 	/// no:doc
 	func removeCardDidFinish(with id: String, result: VGSCheckoutRequestResult) {}
