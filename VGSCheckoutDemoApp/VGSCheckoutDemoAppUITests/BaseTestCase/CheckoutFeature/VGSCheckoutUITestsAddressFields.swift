f2074052947bba0a70781b1cae1813222f41d078
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2022-07-21 07:06:16 +0300

    Add address fields ui tests fields enum

---
+++ VGSCheckoutDemoApp/VGSCheckoutDemoAppUITests/BaseTestCase/CheckoutFeature/VGSCheckoutUITestsAddressFields.swift
@@ -0,0 +1,24 @@
+//
+//  VGSCheckoutUITestsAddressFields.swift
+//  VGSCheckoutDemoAppUITests
+
+import Foundation
+
+/// Defines address fields for UI tests.
+enum VGSCheckoutUITestsAddressFields: String {
+
+	/// Country.
+	case country
+
+	/// Address line 1.
+	case addressLine1
+
+	/// Address line 2.
+	case addressLine2
+
+	/// City.
+	case city
+
+	/// Postal code.
+	case postalCode
+}
